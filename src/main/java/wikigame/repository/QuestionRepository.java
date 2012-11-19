bbaf8a7a54980316c642a0383c133c4e0e95220e
Ville S Peltoniemi <vpeltoni@wop-5.cs.helsinki.fi>
2012-11-19 12:35:10 +0200

    initialisoitu

---
+++ src/main/java/wikigame/repository/QuestionRepository.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikigame.repository;
+
+/**
+ *
+ * @author vpeltoni
+ */
+public class QuestionRepository {
+    
+}
---
+++ src/main/java/wikigame/service/QuestionService.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikigame.service;
+
+/**
+ *
+ * @author vpeltoni
+ */
+public class QuestionService {
+    
+}
