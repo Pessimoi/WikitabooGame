c6fcf7130c892408650b1a8f99556ffd5cbe1a11
Ville S Peltoniemi <vpeltoni@wop-5.cs.helsinki.fi>
2012-11-19 12:44:08 +0200

    blaa

---
+++ src/main/java/wikigame/content/Question.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wikigame.content;
+
+/**
+ *
+ * @author vpeltoni
+ */
+public class Question {
+    
+    private String questionString;
+    private String questioner;
+    
+    
+    
+}
